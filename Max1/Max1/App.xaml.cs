33e275f2537f4c472c2b8deb93bd668494332db3
Max Martinez <max.martinez@tecsup.edu.pe>
2023-04-29 17:48:42 -0600

    Agregar archivos de proyecto.

---
+++ Max1/Max1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Max1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StacklayoutCode();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Max1/Max1/StacklayoutCode.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Max1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StacklayoutCode : ContentPage
+    {
+        public StacklayoutCode()
+        {
+            InitializeComponent();
+            var layout = new StackLayout();
+            var button = new Button
+            {
+                Text = "StackLayoutCode",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            var yellowBox = new BoxView
+            {
+                Color = Color.Green,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            var greemBox = new BoxView
+            {
+                Color = Color.Red,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            var blueBox = new BoxView
+            {
+                Color = Color.SkyBlue,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                HeightRequest = 75
+
+            };
+
+            layout.Children.Add(button);
+            layout.Children.Add(yellowBox);
+            layout.Children.Add(greemBox);
+            layout.Children.Add(blueBox);
+            layout.Spacing = 10;
+            Content = layout;
+        }
+    }
+}
